d2105402184cf07a823453b7b1fb5f2c82daa089
Simone Taeggi <startae14@gmail.com>
2026-04-16 11:07:50 +0200

    08 Tailwind @apply style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,5 +3,14 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./src/**/*.tsx', './src/**/*.ts', './src/**/*.mdx'],
   darkMode: ['class', '[data-mode="dark"]'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          600: '#4338ca', // Add your desired color value here
+        },
+      },
+    },
+  },
   plugins: [],
 } satisfies Config;
