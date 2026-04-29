8e7ee8b5ad7b7ac8e62603d0ac8c0877fc431f0a
Simone Taeggi <startae14@gmail.com>
2026-04-29 09:33:30 +0200

    17 Creating the Callout Component Story

---
+++ src/components/callout/callout.stories.tsx
@@ -0,0 +1,57 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Callout } from './callout';
+
+const meta = {
+  title: 'Components/Callout',
+  component: Callout,
+  args: {
+    children:
+      'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  argTypes: {
+    variant: {
+      control: 'select',
+      options: ['primary', 'information', 'success', 'danger', 'warning'],
+    },
+  },
+} satisfies Meta;
+
+export default meta;
+
+export type Story = StoryObj<typeof Callout>;
+
+export const Primary: Story = {
+  args: {
+    title: 'Primary',
+    variant: 'primary',
+  },
+};
+
+export const Information: Story = {
+  args: {
+    title: 'Information',
+    variant: 'information',
+  },
+};
+
+export const Success: Story = {
+  args: {
+    title: 'Success',
+    variant: 'success',
+  },
+};
+
+export const Danger: Story = {
+  args: {
+    title: 'Danger',
+    variant: 'danger',
+  },
+};
+
+export const Warning: Story = {
+  args: {
+    title: 'Warning',
+    variant: 'warning',
+  },
+};
--- src/components/callout/callout.tsx
+++ src/components/callout/callout.tsx
@@ -8,7 +8,7 @@ type CalloutProps = PropsWithChildren<CalloutVariants> & {
 export const Callout = ({ children, title, variant }: CalloutProps) => {
   return (
     <div className={variants({ variant })}>
-      <h2>{title}</h2>
+      <h2 className="font-semibold">{title}</h2>
       <p>{children}</p>
     </div>
   );
