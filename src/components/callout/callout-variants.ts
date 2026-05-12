391d08135d7226c3e97539ce08e84ed8c38caac3
Simone Taeggi <startae14@gmail.com>
2026-05-12 22:48:33 +0200

    20 Importing Variants

--- src/components/callout/callout-variants.ts
+++ src/components/callout/callout-variants.ts
@@ -2,53 +2,59 @@ import { cva, type VariantProps } from 'class-variance-authority';
 
 export type CalloutVariants = VariantProps<typeof variants>;
 
+const variations = ['primary', 'information', 'success', 'danger', 'warning'] as const;
+
+type Variations = (typeof variations)[number];
+
+const variant = {
+  primary: [
+    'bg-primary-200',
+    'border-primary-500',
+    'text-primary-900',
+    'dark:bg-primary-800',
+    'dark:border-primary-900',
+    'dark:text-primary-50',
+  ],
+  information: [
+    'bg-information-200',
+    'border-information-500',
+    'text-information-900',
+    'dark:bg-information-800',
+    'dark:border-information-900',
+    'dark:text-information-50',
+  ],
+  success: [
+    'bg-success-200',
+    'border-success-500',
+    'text-success-900',
+    'dark:bg-success-800',
+    'dark:border-success-900',
+    'dark:text-success-50',
+  ],
+  danger: [
+    'bg-danger-200',
+    'border-danger-500',
+    'text-danger-900',
+    'dark:bg-danger-800',
+    'dark:border-danger-900',
+    'dark:text-danger-50',
+  ],
+  warning: [
+    'bg-warning-200',
+    'border-warning-500',
+    'text-warning-900',
+    'dark:bg-warning-800',
+    'dark:border-warning-900',
+    'dark:text-warning-50',
+  ],
+};
+
 export const variants = cva(
   // 1. Base styles (condivisi da tutte le varianti)
   ['p-4', 'rounded-lg', 'border', 'shadow-md', 'space-y-4'],
   {
     variants: {
-      variant: {
-        primary: [
-          'bg-primary-200',
-          'border-primary-500',
-          'text-primary-900',
-          'dark:bg-primary-800',
-          'dark:border-primary-900',
-          'dark:text-primary-50',
-        ],
-        information: [
-          'bg-information-200',
-          'border-information-500',
-          'text-information-900',
-          'dark:bg-information-800',
-          'dark:border-information-900',
-          'dark:text-information-50',
-        ],
-        success: [
-          'bg-success-200',
-          'border-success-500',
-          'text-success-900',
-          'dark:bg-success-800',
-          'dark:border-success-900',
-          'dark:text-success-50',
-        ],
-        danger: [
-          'bg-danger-200',
-          'border-danger-500',
-          'text-danger-900',
-          'dark:bg-danger-800',
-          'dark:border-danger-900',
-          'dark:text-danger-50',
-        ],
-        warning: [
-          'bg-warning-200',
-          'border-warning-500',
-          'text-warning-900',
-          'dark:bg-warning-800',
-          'dark:border-warning-900',
-          'dark:text-warning-50',
-        ],
-      },
+      variant,
     },
     // 2. Variante di default
     defaultVariants: {
