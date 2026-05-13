c6283b9f4bb291f26fc63ba6ca4e607ffa01ff44
Simone Taeggi <startae14@gmail.com>
2026-05-13 23:33:28 +0200

    25 Using Decorators for Context

--- src/components/task-list/task-list.stories.tsx
+++ src/components/task-list/task-list.stories.tsx
@@ -1,10 +1,32 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { TaskList } from './task-list';
 
+import { TaskListProvider } from './task-list-context';
+
 const meta = {
   title: 'Components/TaskList',
   component: TaskList,
+  decorators: [
+    (Story, context) => {
+      console.log(context);
+      return (
+        <TaskListProvider
+          tasks={[
+            { id: '1', title: 'Task 1', completed: false },
+            { id: '2', title: 'Task 2', completed: true },
+            { id: '3', title: 'Task 3', completed: false },
+          ]}
+        >
+          <Story />
+        </TaskListProvider>
+      );
+    },
+  ],
 } as Meta<typeof TaskList>;
 
 export default meta;
 type Story = StoryObj<typeof TaskList>;
+
+export const Default: Story = {
+  args: {},
+};
